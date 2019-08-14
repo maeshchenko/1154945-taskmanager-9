2e06ef27e963766f8ad3e57f0a5f2726aa0a1b87
Mikhail Eshchenko <mkeira@yandex-team.ru>
2019-08-14 11:21:02 +0300

    настроил дев-сервер

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,15 @@ const path = require(`path`);
 module.exports = {
   mode: `development`, // Режим сборки
   entry: `./src/main.js`, // Точка входа приложения
-  devtool: `source-map`, // Включаем source-map для удобной отладки
   output: { // Настройка выходного файла
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`, // Включаем source-map для удобной отладки
+  devServer: {
+    contentBase: path.join(__dirname, `public`), // Где искать сборку
+    publicPath: `http://localhost:8080/`, // Веб адрес сборки
+    compress: true, // Сжатие
+    watchContentBase: true // Автоматическая перезагрузка страницы
   }
 };
