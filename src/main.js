f1e6e6ee5689a43f4d7f7efa8805146baae2d9fe
Mikhail Eshchenko <mkeira@yandex-team.ru>
2019-09-02 21:18:26 +0300

    Написал подгрузку карточек по кнопке и вынес моковые данные в helpers

--- src/main.js
+++ src/main.js
@@ -3,64 +3,28 @@ import {createSearchComponent} from './components/SearchComponent';
 import {createFilterComponent} from './components/FilterComponent';
 import {createSortingComponent} from './components/SortingComponent';
 import {createEditTaskCardComponent} from './components/EditTaskCardComponent';
-import {createTaskCardComponent} from './components/TaskCardComponent';
 import {createBoardContainerComponent} from './components/BoardContainerComponent';
 import {createTasksBoardComponent} from './components/TasksBoardComponent';
 import {createLoadMoreButton} from './components/LoadMoreButton';
+import {getTask, createFilters} from "./helpers";
+import {createTaskCardComponent} from "./components/TaskCardComponent";
+
+const tasks = [];
+const tasksNum = 20;
+for (let i = 0; i < tasksNum; i++) {
+  tasks.push(getTask());
+}
 
 const render = (container, component, position) => {
   container.insertAdjacentHTML(position, component);
 };
-const getRandomElements = (arr, num) => arr.sort(() => Math.random() - 0.5).slice(0, Math.floor(Math.random() * num));
-const getTask = () => ({
-  description: [
-    `Изучить теорию`,
-    `Сделать домашку`,
-    `Пройти интенсив на соточку`
-  ][Math.floor(Math.random() * 3)],
-  dueDate: Date.now() + Math.floor(Math.random() * 14 - 7) * 24 * 60 * 60 * 1000,
-  tags: new Set(getRandomElements([
-    `homework`,
-    `theory`,
-    `practice`,
-    `intensive`,
-    `keks`
-  ], 3)),
-  repeatingDays: {
-    'mo': Boolean(Math.round(Math.random())),
-    'tu': Boolean(Math.round(Math.random())),
-    'we': Boolean(Math.round(Math.random())),
-    'th': Boolean(Math.round(Math.random())),
-    'fr': Boolean(Math.round(Math.random())),
-    'sa': Boolean(Math.round(Math.random())),
-    'su': Boolean(Math.round(Math.random()))
-  },
-  color: [
-    `black`,
-    `yellow`,
-    `blue`,
-    `green`,
-    `pink`
-  ][Math.floor(Math.random() * 5)],
-  isFavorite: Boolean(Math.round(Math.random())),
-  isArchive: Boolean(Math.round(Math.random()))
-});
-const filters = [
-  {title: `All`, count:0 },
-  {title: `Overdue`, count: 0},
-  {title: `Today`, count: 0},
-  {title: `Favorites`, count: 1},
-  {title: `Repeating`, count: 0},
-  {title: `Tags`, count: 0},
-  {title: `Archive`, count: 0},
-];
 
 const controlContainer = document.querySelector(`.main__control`);
 const mainContainer = document.querySelector(`.main`);
 
 render(controlContainer, createMenuComponent(), `beforeend`);
 render(mainContainer, createSearchComponent(), `beforeend`);
-render(mainContainer, createFilterComponent(filters), `beforeend`);
+render(mainContainer, createFilterComponent(createFilters(tasks)), `beforeend`);
 render(mainContainer, createBoardContainerComponent(), `beforeend`);
 
 const boardContainer = document.querySelector(`.board`);
@@ -71,7 +35,30 @@ render(boardContainer, createLoadMoreButton(), `beforeend`);
 
 const tasksContainer = document.querySelector(`.board__tasks`);
 
-render(tasksContainer, createEditTaskCardComponent(), `beforeend`);
-for (let i = 0; i < 3; i++) {
-  render(tasksContainer, createTaskCardComponent(getTask()), `beforeend`);
-}
+render(tasksContainer, createEditTaskCardComponent(tasks[0]), `beforeend`);
+
+const loadMore = document.querySelector(`.load-more`);
+
+let taskLoadState = {
+  current: 0,
+  step: 8,
+  max: tasksNum,
+};
+
+export const pushTasks = () => {
+  const {current, step, max} = taskLoadState;
+  taskLoadState.current += step;
+  if (taskLoadState.current > max) {
+    loadMore.style.display = `none`;
+  }
+  const currentTasksArray = tasks.slice(current, current + step);
+  for (let i = 0; i < currentTasksArray.length; i++) {
+    render(tasksContainer, createTaskCardComponent(tasks[i]), `beforeend`);
+  }
+};
+
+loadMore.addEventListener(`click`, () => {
+  pushTasks();
+});
+
+pushTasks();
